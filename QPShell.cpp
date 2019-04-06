545e5e758168d4e8d84e3be9b809b6d51e5c644d
Quentin Barnes <qmb2@gold29.cs.calvin.edu>
2019-04-06 19:13:53 -0400

    Basic commands implemented

--- QPShell.cpp
+++ QPShell.cpp
@@ -11,7 +11,7 @@ void QPShell::run()
        while (notExit)
        {
               Prompt myPrompt;
-              cout << myPrompt.get() << " ";
+              cout << myPrompt.get() << "$ ";
               CommandLine CMD(cin);
               // cout << CMD.getCommand() << "\n"
               //      << CMD.getArgVector(3) << "\n"
@@ -24,13 +24,35 @@ void QPShell::run()
               {
                      notExit = false;
               }
+              else if (command == "cd")
+              {
+                     const char *newDir = CMD.getArgVector(1);
+                     chdir(newDir);
+              }
+              else if (command == "pwd")
+              {
+                     cout << myPrompt.get() << "\n";
+              }
               else
               {
                      // Path myPath;
                      // char *env_args[] = {"PATH=/bin", (char *)0};
                      // execve(, CMD.getArgVector(), env_args);
-                     cout << "Couldn't execute command " << command << "\n"
-                          << flush;
+                     try
+                     {
+                            string wholeCommand = "";
+                            for (int i = 0; i < CMD.getArgCount(); i++)
+                            {
+                                   char *commandBit = CMD.getArgVector(i);
+                                   wholeCommand = wholeCommand + commandBit + " ";
+                            }
+                            system(const_cast<char *>(wholeCommand.c_str()));
+                     }
+                     catch (exception e)
+                     {
+                            cout << "Couldn't execute command " << command << "\n"
+                                 << flush;
+                     }
               }
        }
 }
